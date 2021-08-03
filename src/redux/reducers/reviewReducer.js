3bcd7adc425bf265185b51d1b7024d1ae308d271
Alex Campulungeanu <allecs1987@yahoo.com>
2021-08-03 09:57:15 +0300

    colored diacritics

--- src/redux/reducers/reviewReducer.js
+++ src/redux/reducers/reviewReducer.js
@@ -5,10 +5,6 @@ const initialState = {
 
 const reducer  = (state = initialState, action) => {
   switch (action.type) {
-    // case 'deposit':
-    //     return state + action.payload
-    // case 'withdrway':
-    //   return state - action.payload 
     case 'totalReviews':
       return {...state, totalReviews: action.payload}
     case 'filteredReviews':
