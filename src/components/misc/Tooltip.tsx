ec9a4c379a99df6523a1b05dbcfeb05c42b141af
Alex Campulungeanu <allecs1987@yahoo.com>
2021-11-23 11:39:27 +0000

    fixes

--- src/components/misc/Tooltip.tsx
+++ src/components/misc/Tooltip.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Tippy, {TippyProps} from '@tippyjs/react'
+import Tippy, {TippyProps} from '@tippyjs/react/headless'
 
 export const Tooltip = (props: Partial<TippyProps>): JSX.Element => {
   return (
     <Tippy 
-      {...props} 
+      {...props}
+      // interactive={true}
+      // interactiveBorder={60}
       render={attrs => (
         <div className='bg-gray-600 text-white p-2 rounded-md' {...attrs}>
-          Hello
+          {props.content}
         </div>
       )}
     >
